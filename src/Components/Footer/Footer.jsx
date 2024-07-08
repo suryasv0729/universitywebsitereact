3dc78ec41beb6f1f15bfd1179b5096e31ba2a56d
Surya V <suryasv0729@gmail.com>
2024-07-08 19:30:30 +0530

    footer changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>&copy; 2024 Educity.All rights reserved</p>
+      <p>&copy; 2024 Marina university.All rights reserved</p>
       <ul>
         <li>Terms of Services</li>
         <li>Privacy Policy</li>
